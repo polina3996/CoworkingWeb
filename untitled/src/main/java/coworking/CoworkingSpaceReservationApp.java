5e11d65f125460ef43dbb682d2fc5bdf4efda6a5
PalinaK <polina.kirichencko@yandex.ru>
2025-02-08 22:06:42 +0100

    Docker added and CI/CD pipeline set up
    - Docker file and Docker-compose.yml file were created, containers started and running suscessfully
    - maven.yml file created to check code, set up JDK 21, build and test project, build and run docker image

--- untitled/src/main/java/coworking/CoworkingSpaceReservationApp.java
+++ untitled/src/main/java/coworking/CoworkingSpaceReservationApp.java
@@ -2,10 +2,12 @@ package coworking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "coworking")
+@EntityScan(basePackages = "coworking.model")
 public class CoworkingSpaceReservationApp {
     public static void main(String[] args) {
         SpringApplication.run(CoworkingSpaceReservationApp.class, args);
